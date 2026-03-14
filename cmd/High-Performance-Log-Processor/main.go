ff8ef7f24d689d850090e548373df25ec335c216
Jonathon Chew <jonchew626@hotmail.com>
2026-03-14 19:01:00 +0000

    new: implimentation of MetricsByPath

--- cmd/High-Performance-Log-Processor/main.go
+++ cmd/High-Performance-Log-Processor/main.go
@@ -2,38 +2,56 @@ package main
 
 import (
 	"High-Performance-Log-Processor/internal/cli"
+	"High-Performance-Log-Processor/internal/dashboard"
 	"bufio"
 	"fmt"
 	"log"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // ts=2026-03-14T09:01:20.006Z level=INFO req_id=0f4c9f3d method=GET path=/health status=200 duration_ms=1 bytes=2 ip=10.0.0.5 ua="kube-probe/1.31" msg="request complete"
-type LogLine struct {
-	TS        string
-	Level     string
-	RequestId string
-	Method    string
-	Path      string
-	Status    string
-	Duration  string
-	Bytes     string
-	Ip        string
-	UserAgent string
-	Message   string
+
+var Logs []dashboard.LogRecord
+
+type DateTime struct {
+	Year    int
+	Month   int
+	Day     int
+	Hour    int
+	Minute  int
+	Seconds int
 }
 
-type Logs struct {
-	AllLogs []LogLine
+func StringToInt(st string) int {
+	if strings.Contains(st, ".") {
+		st = st[:strings.IndexByte(st, '.')]
+	}
+	newInt, err := strconv.Atoi(st)
+	if err != nil {
+		return 0
+	}
+	return newInt
+}
 
-	Levels   []string
-	Methods  []string
-	Paths    []string
-	Statuses []string
-	IPs      []string
+func ParseTime(inputTime string) DateTime {
+	// 2026-03-14T09:01:20.006Z
+	timeValue := GetValue(inputTime)
+
+	splitTime := strings.Split(timeValue, "T")
+	dateFields := strings.Split(splitTime[0], "-")
+	timeFields := strings.Split(splitTime[1], ":")
+
+	return DateTime{
+		Year:    StringToInt(dateFields[0]),
+		Month:   StringToInt(dateFields[1]),
+		Day:     StringToInt(dateFields[2]),
+		Hour:    StringToInt(timeFields[0]),
+		Minute:  StringToInt(timeFields[1]),
+		Seconds: StringToInt(timeFields[2]),
+	}
 }
 
 func GetValue(field string) string {
@@ -51,7 +69,6 @@ func GetValue(field string) string {
 func main() {
 
 	var Flags cli.Flags
-	var Logs Logs
 
 	if len(os.Args) > 1 {
 		Flags = cli.CLI(os.Args[1:])
@@ -99,36 +116,28 @@ func main() {
 			log.Panic("The line can not be parsed correctly")
 		}
 
-		var logLine = LogLine{
-			TS:        GetValue(splitLog[0]),
-			Level:     GetValue(splitLog[1]),
-			RequestId: GetValue(splitLog[2]),
-			Method:    GetValue(splitLog[3]),
-			Path:      GetValue(splitLog[4]),
-			Status:    GetValue(splitLog[5]),
-			Duration:  GetValue(splitLog[6]),
-			Bytes:     GetValue(splitLog[7]),
-			Ip:        GetValue(splitLog[8]),
+		logtime, err := time.Parse(time.RFC3339Nano, GetValue(splitLog[0]))
+		if err != nil {
+			log.Println("Unable to parse time!")
+		}
+
+		var logLine = dashboard.LogRecord{
+			TS:         logtime,
+			Level:      GetValue(splitLog[1]),
+			RequestID:  GetValue(splitLog[2]),
+			Method:     GetValue(splitLog[3]),
+			Path:       GetValue(splitLog[4]),
+			Status:     StringToInt(GetValue(splitLog[5])),
+			DurationMS: StringToInt(GetValue(splitLog[6])),
+			Bytes:      StringToInt(GetValue(splitLog[7])),
+			IP:         GetValue(splitLog[8]),
 			// Hint: quoted fields like ua and msg still include their surrounding
 			// quotes at this stage. Trimming those is a separate parsing step.
 			UserAgent: GetValue(splitLog[9]),
 			Message:   GetValue(splitLog[10]),
 		}
 
-		Logs.AllLogs = append(Logs.AllLogs, logLine)
-
-		if !slices.Contains(Logs.Levels, logLine.Level) {
-			Logs.Levels = append(Logs.Levels, logLine.Level)
-		}
-		if !slices.Contains(Logs.Methods, logLine.Method) {
-			Logs.Methods = append(Logs.Methods, logLine.Method)
-		}
-		if !slices.Contains(Logs.Paths, logLine.Path) {
-			Logs.Paths = append(Logs.Paths, logLine.Path)
-		}
-		if !slices.Contains(Logs.Statuses, logLine.Status) {
-			Logs.Statuses = append(Logs.Statuses, logLine.Status)
-		}
+		Logs = append(Logs, logLine)
 	}
 
 	if bufReader.Err() != nil {
@@ -136,18 +145,15 @@ func main() {
 		return
 	}
 
-	var warnings int
-
-	for _, log := range Logs.AllLogs {
-		if log.Level == "WARN" {
-			warnings += 1
-		}
+	for _, i := range dashboard.MetricsByPath(Logs) {
+		fmt.Println(i)
 	}
-	fmt.Print("There were: ", len(Logs.AllLogs), " logs. ", strconv.Itoa(warnings), " were warnings\n")
+
+	/* fmt.Print("There were: ", len(Logs.AllLogs), " logs. ", strconv.Itoa(warnings), " were warnings\n")
 	fmt.Print(
 		"There were: ", len(Logs.Methods), " Methods. ", Logs.Methods, "\n",
 		"There were: ", len(Logs.Levels), " Levels. ", Logs.Levels, "\n",
 		"There were: ", len(Logs.Paths), " Paths. ", Logs.Paths, "\n",
 		"There were: ", len(Logs.Statuses), " Statuses. ", Logs.Statuses, "\n",
-	)
+	) */
 }
