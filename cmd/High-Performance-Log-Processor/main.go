8573c0915af7e0a19ba55c3f41c81b1f3f5b6305
Jonathon Chew <jonchew626@hotmail.com>
2026-03-21 10:51:51 +0000

    new: commands for CLI to choose how to parse logs

--- cmd/High-Performance-Log-Processor/main.go
+++ cmd/High-Performance-Log-Processor/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
 	"High-Performance-Log-Processor/internal/cli"
 	"High-Performance-Log-Processor/internal/dashboard"
 	"High-Performance-Log-Processor/internal/parseinput"
-	"encoding/json"
-	"os"
 )
 
 // ts=2026-03-14T09:01:20.006Z level=INFO req_id=0f4c9f3d method=GET path=/health status=200 duration_ms=1 bytes=2 ip=10.0.0.5 ua="kube-probe/1.31" msg="request complete"
+
+func checkTimeDurationSet(inputTime time.Duration) time.Duration {
+
+	if inputTime == 0 {
+		bucket, err := time.ParseDuration("5m")
+		if err != nil {
+			log.Panic("[ERROR]: No time duration was found and could not be forced!")
+		}
+		return bucket
+	}
+
+	return 0
+}
 
 func main() {
 
@@ -25,15 +42,119 @@ func main() {
 	}
 
 	if Flags.FileName == "" {
-		os.Exit(1)
+		log.Panic("[ERROR]: No file name found")
 	} else {
 		Logs = parseinput.ParseFile(Flags)
+		switch {
+		default:
+			log.Panic("[ERROR]: No option of how to parse file")
+			os.Exit(1)
+		case Flags.MetricsByPath == true:
+			for _, i := range dashboard.MetricsByPath(Logs) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.LatencyByPath == true:
+			for _, i := range dashboard.LatencyByPath(Logs) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.SlowRequestsByPath == true:
+			for _, i := range dashboard.SlowRequestsByPath(Logs) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.ErrorRateByPath == true:
+			for _, i := range dashboard.ErrorRateByPath(Logs) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.RequestsByWindow == true:
+			Flags.Bucket = checkTimeDurationSet(Flags.Bucket)
+			for _, i := range dashboard.RequestsByWindow(Logs, dashboard.BucketSize(Flags.Bucket)) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.LevelsByWindow == true:
+			Flags.Bucket = checkTimeDurationSet(Flags.Bucket)
+			for _, i := range dashboard.LevelsByWindow(Logs, dashboard.BucketSize(Flags.Bucket)) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.WarnAndErrorCountsByWindow == true:
+			Flags.Bucket = checkTimeDurationSet(Flags.Bucket)
+			for _, i := range dashboard.WarnAndErrorCountsByWindow(Logs, dashboard.BucketSize(Flags.Bucket)) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.StatusClassesByWindow == true:
+			Flags.Bucket = checkTimeDurationSet(Flags.Bucket)
+			for _, i := range dashboard.StatusClassesByWindow(Logs, dashboard.BucketSize(Flags.Bucket)) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.StatusCodesByWindow == true:
+			Flags.Bucket = checkTimeDurationSet(Flags.Bucket)
+			for _, i := range dashboard.StatusCodesByWindow(Logs, dashboard.BucketSize(Flags.Bucket)) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.MetricsByPathAndWindow == true:
+			Flags.Bucket = checkTimeDurationSet(Flags.Bucket)
+			for _, i := range dashboard.MetricsByPathAndWindow(Logs, dashboard.BucketSize(Flags.Bucket)) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.SlowRequestsByWindow == true:
+			Flags.Bucket = checkTimeDurationSet(Flags.Bucket)
+			for _, i := range dashboard.SlowRequestsByWindow(Logs, dashboard.BucketSize(Flags.Bucket)) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		case Flags.ErrorRateByWindow == true:
+			Flags.Bucket = checkTimeDurationSet(Flags.Bucket)
+			for _, i := range dashboard.ErrorRateByWindow(Logs, dashboard.BucketSize(Flags.Bucket)) {
+				if Flags.Output == "JSON" {
+					json.NewEncoder(os.Stdout).Encode(i)
+				} else {
+					fmt.Println(i)
+				}
+			}
+		}
+
 	}
 
-	for _, i := range dashboard.MetricsByPath(Logs) {
-		err := json.NewEncoder(os.Stdout).Encode(i)
-		if err != nil {
-			continue
-		}
-	}
 }
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -9,16 +9,31 @@ import (
 const version string = "0.0.2"
 
 type Flags struct {
-	FileName string
-	Ping     bool
-	Bucket   time.Duration
+	FileName                   string
+	Ping                       bool
+	Bucket                     time.Duration
+	Output                     string
+	MetricsByPath              bool
+	LatencyByPath              bool
+	SlowRequestsByPath         bool
+	ErrorRateByPath            bool
+	RequestsByWindow           bool
+	LevelsByWindow             bool
+	WarnAndErrorCountsByWindow bool
+	StatusClassesByWindow      bool
+	StatusCodesByWindow        bool
+	MetricsByPathAndWindow     bool
+	SlowRequestsByWindow       bool
+	ErrorRateByWindow          bool
 }
 
 func CLI(args []string) Flags {
 
 	var returnFlags Flags
 
-	for _, arg := range args {
+	for index := 0; index < len(args); index++ {
+		arg := args[index]
+
 		switch arg {
 		default:
 			if _, err := os.Lstat(arg); err != nil {
@@ -29,16 +44,57 @@ func CLI(args []string) Flags {
 			os.Stderr.Write([]byte("USEAGE: pass in a file name"))
 		case "version":
 			os.Stderr.Write([]byte("Version: " + version))
+		case "output", "--output", "-o", "-output":
+			if len(args) >= index+1 {
+
+			} else {
+				log.Panic("[ERROR]: No output detected")
+			}
 		case "ping":
 			return Flags{
 				Ping: true,
 			}
 		case "--time":
-			bucket, err := time.ParseDuration(arg)
-			if err != nil {
-				log.Fatal("Unable to parse time: ", err)
+			if len(args) >= index+1 {
+				bucket, err := time.ParseDuration(args[index+1])
+				if err != nil {
+					log.Fatal("Unable to parse time: ", err)
+				}
+				returnFlags.Bucket = bucket
+				index++
+			} else {
+				log.Print("[waRNING]: No time period entered, defaulting to 5 minutes")
+				bucket, err := time.ParseDuration("5m")
+				if err != nil {
+					log.Fatal("Unable to parse time: ", err)
+				}
+				returnFlags.Bucket = bucket
 			}
-			returnFlags.Bucket = bucket
+			index += 1
+		case "MetricsByPath":
+			returnFlags.MetricsByPath = true
+		case "LatencyByPath":
+			returnFlags.LatencyByPath = true
+		case "SlowRequestsByPath":
+			returnFlags.SlowRequestsByPath = true
+		case "ErrorRateByPath":
+			returnFlags.ErrorRateByPath = true
+		case "RequestsByWindow":
+			returnFlags.RequestsByWindow = true
+		case "LevelsByWindow":
+			returnFlags.LevelsByWindow = true
+		case "WarnAndErrorCountsByWindow":
+			returnFlags.WarnAndErrorCountsByWindow = true
+		case "StatusClassesByWindow":
+			returnFlags.StatusClassesByWindow = true
+		case "StatusCodesByWindow":
+			returnFlags.StatusCodesByWindow = true
+		case "MetricsByPathAndWindow":
+			returnFlags.MetricsByPathAndWindow = true
+		case "SlowRequestsByWindow":
+			returnFlags.SlowRequestsByWindow = true
+		case "ErrorRateByWindow":
+			returnFlags.ErrorRateByWindow = true
 		}
 	}
 
